5f5b3b8c4a432643cf0ed819e87e3528f408f749
walker <warriorsfly@gmail.com>
2020-01-06 10:47:40 +0800

    add color white

--- lib/styles.dart
+++ lib/styles.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 abstract class Styles {
   static const Color colorPrimary = Color(0xFFFF8126);
   static const Color colorPrimaryDark = Color(0xFFFF117E);
+  static const Color colorWhite = Color(0xFFFFFFFF);
 
-  static const TextStyle small_normal_10 = TextStyle(fontSize: 10);
+  static const TextStyle small_normal = TextStyle(fontSize: 12);
 }
