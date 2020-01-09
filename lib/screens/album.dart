4cad859c7eda10fc4594d22642a43fe78b1deb6e
walker <warriorsfly@gmail.com>
2020-01-09 17:53:29 +0800

    add album screen

---
+++ lib/screens/album.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AlbumScreen extends StatefulWidget {
+  @override
+  _AlbumScreenState createState() => _AlbumScreenState();
+}
+
+class _AlbumScreenState extends State<AlbumScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+} 
