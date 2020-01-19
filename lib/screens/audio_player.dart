e628de644705e25195841dae46125567855fe7f4
walker <warriorsfly@gmail.com>
2020-01-19 08:53:08 +0800

    add audio player

---
+++ lib/screens/audio_player.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AudioPlayerScreen extends StatefulWidget {
+  @override
+  _AudioPlayerScreenState createState() => _AudioPlayerScreenState();
+}
+
+class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
