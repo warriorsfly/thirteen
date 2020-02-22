6f2c61188739e972d67597452ee807ee67f8b070
walker <warriorsfly@gmail.com>
2020-02-22 16:18:14 +0800

    delete useless file

--- lib/player/simple_audioplayer.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:thirteen/player/base_player.dart';
-
-mixin SimpleAudioPlayerMixin<T extends StatefulWidget> on State<T> implements BaseAudioPlayer{
-
-}
